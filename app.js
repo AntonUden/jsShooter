a82ab0252f5b6c445872061e90088309bbe759f1
BuildTools <udenanton@hotmail.com>
2018-05-28 08:44:06 +0200

    Changed let c = Math.sqrt( a*a + b*b ); return c; to return Math.sqrt( a*a + b*b );

--- app.js
+++ app.js
@@ -459,8 +459,7 @@ function getDistance(x1, y1, x2, y2) {
 	let a = x1 - x2;
 	let b = y1 - y2;
 
-	let c = Math.sqrt( a*a + b*b );
-	return c;
+	return Math.sqrt( a*a + b*b );
 }
 
 function getSmallest(obj) {
